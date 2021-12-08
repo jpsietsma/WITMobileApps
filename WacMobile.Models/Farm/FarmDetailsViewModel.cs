c150bf045f2633a9ea6b297ba6494ef8f7770456
jpsietsma <jpsietsma@gmail.com>
2021-12-08 14:12:31 -0500

    Added WacMobile.Models project class

---
+++ WacMobile.Models/Farm/FarmDetailsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WacMobile.Models.Farm
+{
+    public class FarmDetailsViewModel
+    {
+        [Key]
+        public int farmID { get; set; }
+        public int? farmContactID { get; set; }
+        public string farmName { get; set; }
+        public string farmFADStatus { get; set; }
+        public string farmRankScore { get; set; }
+        public string farmAEMScore { get; set; }
+        public int? farmT1AssignedToID { get; set; }
+        public int? farmT2AssignedToID { get; set; }
+        public string farmTier2Notes { get; set; }
+        public string farmIdentificationAlpha { get; set; }
+        public string farmIdentificationNumber { get; set; }
+        public string farmIdentificationValue { get; set; }    
+        public string farmPrimaryTaxParcelID { get; set; }
+        public int? farmPrimaryBasinID { get; set; }
+        
+        [Column(TypeName ="nchar(10)")]
+        public string farmPrimarySubBasinID { get; set; }
+
+        public int? farmPrimaryAddressID { get; set; }
+        public int? farmParticipantProducerID { get; set; }
+        public int? farmCreatedByID { get; set; }
+        public DateTime? farmCreatedDate { get; set; }
+        public int? farmModifiedByID { get; set; }
+        public DateTime? farmModifiedDate { get; set; }
+        public string farmSizeConstant { get; set; }
+        public decimal? farmAvgFieldDistanceFromWatercourse { get; set; }
+        public decimal? farmFarmsteadDistanceFromWatercourse { get; set; }
+        public decimal? farmProfessionalJudgementPoints { get; set; }
+        public string farmEohPLevel { get; set; }
+        public DateTime? farmRankingDetailsUpdatedDate { get; set; }
+        public int? farmRankingDetailsUpdatedByID { get; set; }
+
+    }
+}
